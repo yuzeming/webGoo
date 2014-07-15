bc8c52cb415661d5e81587ff4956b28fc8a08a10
Zeming YU <yuzeming@gmail.com>
2014-07-15 22:06:32 +0800

    no message

--- test.js
+++ test.js
@@ -1,127 +1,198 @@
-var goo = {};
-goo.img = new Image();
-goo.img.onload = init;
-goo.img.src = "ball.png";
-goo.size = 10;
-goo.r = goo.size / 2;
-goo.maxlen = 20;
-goo.maxlink = 2;
+var goo = {
+    url:  "ball.png",
+    size: 32,
+    r: this.size / 2,
+    maxlen: 400,
+    maxlink: 2
+};
 
-var spring = {};
-spring.img = new Image();
-spring.img.scr = "spring_goo.png";
-
-var canvas = document.getElementById("canvas");
-
-var ctx = canvas.getContext("2d");
-
-function Link(_len,_k,_node){
-    this["len"] = _len || 10;
-    this["k"] = _k || 1;
-    this["node"] = _node || [];
-    this.draw = function(ctx) {
-        if (this.node.length == 2){
-            ctx.set
-            ctx.beginPath();
-            ctx.moveTo(this.node[0].location.x,this.node[0].location.y);
-            ctx.lineTo(this.node[1].location.x,this.node[1].location.y);
-            ctx.closePath();
-            ctx.stroke();
-        }
-    }
-}
-
-function Ball(_x,_y,_state,_link){
-    this.type = goo;
-    this.location = {"x":_x || 0, "y": _y || 0};
-    this.state =_state || "fixed";
-    this.link = _link || [];
-    this.connect = function( _len,_k,_ball1,_ball2){
-        if (!_ball2) { _ball2 = this; }
-        var tmp = new Link(_len,_k,[ _ball1, _ball2 ]);
-        _ball1.link.push(tmp);
-        _ball2.link.push(tmp);
-        links.push(tmp);
-    };
-    this.draw = function(ctx){
-        ctx.drawImage(this.type.img,this.location.x-this.type.r,this.location.y-this.type.r,this.type.size,this.type.size);
-    }
-}
-
-
-var balls = [];
-var links = [];
-
-function init(){
-    balls.push( new Ball(10,10,"fixed") );
-    balls.push( new Ball(10,40,"fixed") );
-    balls.push( new Ball(36,25,"fixed") );
-    balls.push( new Ball(40,40,"active") );
-
-    balls[0].connect(10,1,balls[1]);
-    balls[1].connect(10,1,balls[2]);
-    balls[2].connect(10,1,balls[0]);
-    setInterval(draw,10);
-}
-
-function draw(){
-    ctx.clearRect(0,0,800,600);
-    for (var x in balls){
-        balls[x].draw(ctx);
-    }
-    for (var x in links){
-        links[x].draw(ctx);
-    }
-}
-
-function getPointOnCanvas( e ) {
-    var box =canvas.getBoundingClientRect();
-    return {
-        x: e.clientX - box.left ,
-        y: e.clientY - box.top
-    };
-}
-
-function showCanLink(y) {
-    for (var x in balls){
-        if (balls[x].state == "fixed" && && (dist(balls[x].location,balls[y].location) <=balls[y].type.r)){
-
-        }
-    }
-}
+var spring = {
+    url: "spring.png",
+    width:32
+};
 
 function sqr(x) { return x*x; }
 
 function dist(a,b){
+    if (a instanceof Ball) { a = a.location }
+    if (b instanceof Ball) { b = b.location }
     return Math.sqrt(sqr(a.x- b.x) + sqr(a.y - b.y));
 }
 
-var nowSelect = -1;
+function DomDiv(_class) {
+    var ret = document.createElement("div");
+    if (_class){
+        ret.classList.add(_class)
+    }
+    var gamebox = document.getElementById("gamebox").appendChild(ret);
+    return ret;
+}
 
-function mouseDown( e ){
-    var pos = getPointOnCanvas(e);
+function Link(_ball1,_ball2,_class){
+    this.node = [_ball1,_ball2];
+    this.len = dist(_ball1,_ball2);
+    this.dom = new DomDiv(_class||"spring");
+    this.dom.x=this;
+    this.update = function(){
+        if (this.node.length!=2){
+            return null;
+        }
+
+        var a = this.node[0].location, b = this.node[1].location;
+
+        var h = dist(a,b)-4;
+        var w = 16;
+
+        this.dom.style["height"] = h + "px";
+        var val = {
+            x : ( a.x + b.x - w + 4) / 2,
+            y : ( a.y + b.y - h + 4) / 2
+        };
+        var deg =- Math.atan2(a.x - b.x, a.y - b.y)*180/Math.PI;
+        this.dom.style["-webkit-transform"] = "translate("+val.x+"px,"+val.y+"px) rotate("+deg+"deg)";
+    };
+    if (this.node.length == 2){
+        this.update();
+    }
+}
+
+function Ball(_x,_y,_state,_link){
+
+    this.dom = new DomDiv("ball");
+    this.dom.x=this;
+
+    this.__defineSetter__("location",function(val){
+        this._location = val;
+        this.dom.style["-webkit-transform"] = "translate("+(val.x-16)+"px,"+(val.y-16)+"px)";
+        for (var x in this.link){
+            this.link[x].update();
+        }
+    });
+
+    this.__defineGetter__("location",function(){return this._location;});
+
+    this.location = {"x":_x || 0, "y": _y || 0};
+    this.state =_state || "fixed";
+    this.link = _link || [];
+    this.connect = function(_ball1,_ball2){
+        if (!_ball2) { _ball2 = this; }
+        var tmp = new Link( _ball1, _ball2 );
+        _ball1.link.push(tmp);
+        _ball2.link.push(tmp);
+        links.push(tmp);
+        tmp.update();
+    };
+
+    this.hasLinkto = function(_ball1,_ball2){
+        if (!_ball2) { _ball2 = this; }
+        for (var x in _ball2.link){
+            if (_ball2.link[x].node[0] == _ball1 || _ball2.link[x].node[1] == _ball1){
+                return _ball2.link[x];
+            }
+        }
+        return false;
+    }
+
+}
+
+var balls = [];
+var links = [];
+
+function init() {
+    balls.push(new Ball(100, 100, "fixed"));
+    balls.push(new Ball(100, 400, "fixed"));
+    balls.push(new Ball(360, 250, "fixed"));
+
+    for (var x = 0; x < 20; ++x) {
+
+        balls.push(new Ball(400+x*20, 400+x*20, "active"));
+
+    }
+
+    balls[0].connect(balls[1]);
+    balls[1].connect(balls[2]);
+    balls[2].connect(balls[0]);
+}
+
+var tmp_links = [];
+
+function showCanLink( b ) {
+    tmp_links = [];
     for (var x in balls){
-        if (balls[x].state == "active" && (dist(balls[x].location,pos) <=balls[x].type.r) ) {
-            nowSelect = x;
-            console.log("now select:" + x);
-            break;
+        if (balls[x].state=="fixed" && dist(b, balls[x]) <= goo.maxlen){
+            var tmp = balls[x];
+            var tmp_dist = dist(tmp, b);
+            for (var y=0;y<goo.maxlink&&tmp!=null;++y){
+                if (tmp_links[y]==null || tmp_dist < dist(tmp_links[y],b) ){
+                    var t = tmp_links[y] || null;
+                    tmp_links[y] = tmp;
+                    tmp = t;
+                    if (t) {
+                        tmp_dist = dist(tmp, b);
+                    }
+                }
+            }
         }
     }
-}
 
-function mouseMove( e ){
-    var pos = getPointOnCanvas(e);
-    if (nowSelect != -1){
-        balls[nowSelect].location = pos;
-        showCanLink(nowSelect);
+    $("div.spring_x").remove();
+
+    if (tmp_links.length > 1) {
+        if (tmp_links[0].hasLinkto(tmp_links[1])) {
+            if (dist(tmp_links[0],tmp_links[1])*1.05<dist(tmp_links[0],now_select)+dist(tmp_links[1],now_select)){
+                for (var x in tmp_links) {
+                    new Link(tmp_links[x], b, "spring_x");
+                    tmp_links.beLink = false;
+                }
+            } else {
+                tmp_links = [];
+                tmp_links.beLink = false;
+            }
+        } else {
+            if (dist(tmp_links[0],tmp_links[1]) <= goo.maxlen && dist(tmp_links[0],tmp_links[1])*1.2>=dist(tmp_links[0],b)+dist(tmp_links[1],b) ) {
+                new Link(tmp_links[0], tmp_links[1], "spring_x");
+                tmp_links.beLink = true;
+            } else {
+                tmp_links = [];
+                tmp_links.beLink = false;
+            }
+        }
     }
+    b.dom.style["display"] = tmp_links.beLink?"none":"block";
 }
 
-function mouseUp( e ){
-    var pos =getPointOnCanvas( e );
-    nowSelect = -1;
-}
+var now_select = null;
 
-canvas.addEventListener("mousedown",mouseDown);
-canvas.addEventListener("mousemove",mouseMove);
-canvas.addEventListener("mouseup",mouseUp);
+$("#gamebox").on("mousedown","div.ball",null,function(e) {
+    if (this.x.state == "active") {
+        now_select = this.x;
+    }
+});
+
+$("#gamebox").on("mousemove",function(e) {
+    if (now_select){
+        now_select.location = {"x": e.clientX,"y":e.clientY};
+        showCanLink(now_select);
+    }
+});
+
+$("#gamebox").on("mouseup",function(e) {
+
+    if (now_select && tmp_links.length == 2){
+        $("div.spring_x").remove();
+        if (tmp_links.beLink){
+            tmp_links[0].connect(tmp_links[1]);
+            now_select.state = "hide";
+        } else {
+            for (var x = 0; x < tmp_links.length; ++x) {
+                now_select.connect(tmp_links[x]);
+            }
+            now_select.state = "fixed";
+        }
+    }
+
+    now_select = null;
+});
+
+
+init();
